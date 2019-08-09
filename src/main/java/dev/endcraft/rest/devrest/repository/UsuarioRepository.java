4ba53275af7a4829ae951e8114ceeb60a85d00ed
Jonas <jonas@swa.com.br>
2019-08-09 09:14:08 -0300

    #3

---
+++ src/main/java/dev/endcraft/rest/devrest/repository/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package dev.endcraft.rest.devrest.repository;
+
+import dev.endcraft.rest.devrest.entitys.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {}
